d75e902db9f63ec7206636f396aa666b3b226046
HuangJijin <jijin.huang2012@gmail.com>
2012-12-12 19:34:40 +0800

    Protecting edit and update with a beford filter.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
 
 	def show
 		@user = User.find(params[:id])
@@ -22,12 +23,12 @@ class UsersController < ApplicationController
 	  	render 'new'
 	  end
   end
-
+  # 2012-12-12/17:00
   def edit
     @user = User.find(params[:id])
     @title = "Edit user"
   end
-
+  # 2012-12-12/19:00
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
@@ -38,4 +39,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+  def authenticate
+    # flash[:notice] = "Please sign in to access this page." 
+    # redirect_to signin_path, :notice => "Please sign in to access this page." 
+    deny_access unless signed_in? #将上面的方法提取到deny_access，并放置在sessions_helper.rb 里(因为controller里，application_controller.rb 引用了它  )
+  end
+
 end
