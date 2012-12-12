84fd21bed6a7240d79b9021fc8e5890d248f3cfa
HuangJijin <jijin.huang2012@gmail.com>
2012-12-12 10:49:41 +0800

    Handing invalid signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	
+  	user = User.authenticate(params[:session][:email], params[:session][:password])
+  	if user.nil?
+  		@title = "Sign in"
+  		flash.now[:error] = "Invalid email/password combination."
+  		render 'new'
+  	else
+  		# success
+  	end
   end
 
-  def destroy
+  def destroy 
   	
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,10 +8,35 @@ describe SessionsController do
       get :new
       response.should be_success
     end
-
+    # 2012-12-11/22:30
     it "should have a right title" do
     	get :new
     	response.should have_selector('title', :content => "Sign in")
+    end
+  end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :email => "", :password => ""}
+      end
+
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should have the title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => "Sign in")
+      end
+
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
     end
   end
 end
