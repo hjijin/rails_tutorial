3cc4fb84a74fe1b3e2441f0595deda9346ba307a
jijin <jijin.huang2012@gmail.com>
2013-06-03 20:54:34 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 # source 'http://ruby.taobao.org'
 
 gem 'rails', '3.2.13'
-
+gem 'bootstrap-sass', '2.3.1.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 RailsTutorial::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+  # get "static_pages/home" # 定义具名路由:
+  # match '/home', to: 'static_pages#home' # match '/home' 会自动创建具名路由函数
+  # home_path => '/home'
+  # home_url  => 'http://localhost:3000/home'
+  root to: 'static_pages#home'
+  
+  # get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "/static_pages/about"
+  # get "/static_pages/about"
+  match '/about', to: 'static_pages#about'
+
+  # get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signup',  to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
