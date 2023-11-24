2a8b2cdd9f33ec0b977fc89d3307962d96531c0f
jim.h <jim.h@shopperplus.com>
2023-11-24 16:41:49 +0800

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # assert_select 'div#<CSS id for error explanation>'
     # assert_select 'div.<CSS class for field with error>'
   end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
