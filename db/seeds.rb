65f2036b98df01cf4bf706b8910c5eeefc6a5043
jim.h <jim.h@shopperplus.com>
2023-11-28 16:04:02 +0800

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,15 @@
 #   end
 
 # Create a main sample user.
-User.create!(name:  "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(
+  name:  "Example User", 
+  email: "example@railstutorial.org", 
+  password: "foobar", 
+  password_confirmation: "foobar", 
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
+)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -19,5 +27,8 @@ User.create!(name:  "Example User", email: "example@railstutorial.org", password
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now
+              )
 end
