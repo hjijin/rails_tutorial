104c0936771c0a25efdaa78ef4f42530c64c3c77
jim.h <jim.h@shopperplus.com>
2023-11-30 15:25:03 +0800

    Add password reset.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,13 +10,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#home"
 
-  get  "/help",    to: "static_pages#help"
-  get  "/about",   to: "static_pages#about"
-  get  "/contact", to: "static_pages#contact"
-  get  "/signup",  to: "users#new"
+  get    "/help",    to: "static_pages#help"
+  get    "/about",   to: "static_pages#about"
+  get    "/contact", to: "static_pages#contact"
+  get    "/signup",  to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
+  
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
